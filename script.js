9de3e57dfcc375b18deadaafa1cc2b37a7d944a6
Berckson Thierry <abreuthierry3@gmail.com>
2024-09-11 12:46:09 -0300

    script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let soma = number + número
 let subtração = number - número
 let multiplicação = number * número
 let divisão = number / número
-alert (number + "+" + número + "=" + soma + "\n" + number + "-" + número + "=" + subtração + "\n" + number + "*" + número + "=" + multiplicação + "\n" + number + "/" + número + "=" + divisão)
-
+alert(number + "+" + número + "=" + soma + "\n" + number + "-" + número + "=" + subtração + "\n" + number + "*" + número + "=" + multiplicação + "\n" + number + "/" + número + "=" + divisão)
 //cadastramento de recrutas
 let nome = window.prompt("qual seu nome?")
 let sobrenome = window.prompt("qual seu sobrenome?")
@@ -16,4 +15,4 @@ let idade = window.prompt("qual sua data de nascimento")
 let curso = window.prompt("qual seu curso?")
 //window.alert (" olá, " + "  nome  + sobrenome")
 let nascimento = (2024 - idade)
-window.alert (' olá, ' + ' ' +  nome + ' ' + sobrenome + ' ' + ' você estuda ' + ' ' + curso  + ' ' + 'e tem '+ ' ' + nascimento + ' ' + 'anos.'  +' ' +  'legal!')
+window.alert(' olá, ' + ' ' +  nome + ' ' + sobrenome + ' ' + ' você estuda ' + ' ' + curso  + ' ' + 'e tem '+ ' ' + nascimento + ' ' + 'anos.'  +' ' +  'legal!')
